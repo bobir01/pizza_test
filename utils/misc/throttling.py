5ff8d9cdc1b84279f89a1f6c5569c18b43eef4ae
Bobir <68824641+bobir01@users.noreply.github.com>
2022-05-23 12:33:07 +0500

    Update throttling.py

--- utils/misc/throttling.py
+++ utils/misc/throttling.py
@@ -2,10 +2,7 @@ def rate_limit(limit: int, key=None):
     """
     Decorator for configuring rate limit and key in different functions.
 
-    :param limit:
-    :param key:
-    :return:
-    """
+    
 
     def decorator(func):
         setattr(func, 'throttling_rate_limit', limit)
